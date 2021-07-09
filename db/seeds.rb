c17595d3b7364a2d7a6ba12f38df1c9230929ddc
George Kettle <georgekettle.gk@gmail.com>
2021-07-09 16:43:56 +1000

    Created seed file for users, groups and group members

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating users"
+george = User.create!(email: "george@gmail.com", password: "secret", full_name: "George Kettle", display_name: "George")
+jason = User.create!(email: "jason@gmail.com", password: "secret", full_name: "Jason Irving", display_name: "Jason")
+filia = User.create!(email: "filia@gmail.com", password: "secret", full_name: "Filia Birkners", display_name: "Filia")
+ante = User.create!(email: "ante@gmail.com", password: "secret", full_name: "Ante Strika", display_name: "Ante")
+
+users = [george, jason, filia, ante]
+puts "Finished creating users"
+
+puts "Creating groups"
+academy_group = Group.create!(name: "The Academy")
+puts "Finished creating groups"
+
+puts "Creating group members"
+users.each do |user|
+  role = :member
+  role = :editor if user.email == "filia@gmail.com"
+  role = :admin if user.email == "jason@gmail.com"
+  GroupMember.create!(group: academy_group, user: user, role: role)
+end
+puts "Finished creating group members"
