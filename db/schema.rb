1054b5c9be1f7464634349f7368fa462fcfe6dc1
George Kettle <georgekettle.gk@gmail.com>
2021-07-09 16:47:19 +1000

    permissions created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_09_062342) do
+ActiveRecord::Schema.define(version: 2021_07_09_064601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2021_07_09_062342) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "permissions", force: :cascade do |t|
+    t.string "title", null: false
+    t.bigint "group_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_id"], name: "index_permissions_on_group_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -47,4 +55,5 @@ ActiveRecord::Schema.define(version: 2021_07_09_062342) do
 
   add_foreign_key "group_members", "groups"
   add_foreign_key "group_members", "users"
+  add_foreign_key "permissions", "groups"
 end
