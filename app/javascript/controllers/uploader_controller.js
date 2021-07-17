350f89c1df991bb47a4e457175fba4634f6b73d4
George Kettle <georgekettle.gk@gmail.com>
2021-07-17 19:56:42 +1000

    updates to catch errors when uploading avatar (could be cleaner)

--- app/javascript/controllers/uploader_controller.js
+++ app/javascript/controllers/uploader_controller.js
@@ -4,7 +4,7 @@ import * as FilePond from 'filepond';
 // Import the plugin code
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import FilePondPluginFileEncode from 'filepond-plugin-file-encode';
-import FilePondPluginImageEdit from 'filepond-plugin-image-edit';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
 // Import the plugin styles
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 
@@ -12,7 +12,7 @@ import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 FilePond.registerPlugin(
   FilePondPluginImagePreview,
   FilePondPluginFileEncode,
-  FilePondPluginImageEdit
+  FilePondPluginFileValidateType
 );
 
 export default class extends Controller {
@@ -22,10 +22,14 @@ export default class extends Controller {
     this.form = this.element.form;
 
     this.pond = FilePond.create(this.element, {
+      labelIdle: `Drag & Drop your picture or <span class="filepond--label-action">Browse</span>`,
+      acceptedFileTypes: ['image/png', 'image/jpeg', 'image/gif'],
       server: {
           process: this.uploadFile,
       },
     });
+    const pqinaLogo = document.querySelector('.filepond--credits');
+    pqinaLogo && pqinaLogo.remove();
   }
 
   uploadFile(fieldName, file, metadata, load, error, progress, abort) {
