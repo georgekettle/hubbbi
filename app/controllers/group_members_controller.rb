fea17beeee30beec459e704420a7b13300766a71
George Kettle <georgekettle.gk@gmail.com>
2021-07-18 19:14:02 +1000

    can view all group members within a group

--- app/controllers/group_members_controller.rb
+++ app/controllers/group_members_controller.rb
@@ -3,4 +3,13 @@ class GroupMembersController < ApplicationController
     @group_member = GroupMember.find(params[:id])
     authorize @group_member
   end
+
+  def index
+    @group = Group.find(params[:group_id])
+    @group_members = @group.group_members
+  end
+
+  def new
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :groups do
+    resources :group_members, only: [:index, :new]
     member do
       get :settings
     end
@@ -10,7 +11,7 @@ Rails.application.routes.draw do
       get :edit_avatar
     end
   end
-  resources :group_members, only: :show
+  resources :group_members, only: [:show]
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
