68209ad10c2514a151ae31a5536f00f696dfce9b
George Kettle <georgekettle.gk@gmail.com>
2021-07-18 21:14:58 +1000

    styled new page for submitting user invite

--- app/controllers/group_members_controller.rb
+++ app/controllers/group_members_controller.rb
@@ -1,15 +1,23 @@
 class GroupMembersController < ApplicationController
+  before_action :set_group, only: [:new, :index]
+
   def show
     @group_member = GroupMember.find(params[:id])
     authorize @group_member
   end
 
   def index
-    @group = Group.find(params[:group_id])
     @group_members = @group.group_members
   end
 
   def new
+    @group_member = GroupMember.new(group: @group)
+    authorize @group_member
+  end
 
+  private
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
