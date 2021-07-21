ec95a8ce9c5e9f4ba9b9976fd7fc68dd08ead82e
George Kettle <georgekettle.gk@gmail.com>
2021-07-21 15:14:14 +1000

    example placeholders for email, name and password set

---
+++ app/helpers/user_helper.rb
@@ -0,0 +1,13 @@
+module UserHelper
+  def example_email
+    "michelle_obama@mail.com"
+  end
+
+  def example_name
+    "Michelle Obama"
+  end
+
+  def example_password
+    "********"
+  end
+end
