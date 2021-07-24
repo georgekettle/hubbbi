93e531737704be88cd70577de6cef87a7caa16ca
George Kettle <georgekettle.gk@gmail.com>
2021-07-24 14:28:29 +1000

    added uniqueness validation to course members

--- app/models/course_member.rb
+++ app/models/course_member.rb
@@ -1,4 +1,6 @@
 class CourseMember < ApplicationRecord
   belongs_to :course
   belongs_to :group_member
+
+  validates :group_member, uniqueness: { scope: :course, message: 'Is already in this course' }
 end
