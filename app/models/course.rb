d4eb9fbe9344228688b9f62418ccbdd8b2b7bca4
George Kettle <georgekettle.gk@gmail.com>
2021-07-24 15:10:41 +1000

    can view list of courses on group show page

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,8 @@ class Course < ApplicationRecord
   has_many :course_members, dependent: :destroy
   has_many :group_members, through: :course_members
 
+  has_one_attached :cover
+
   # status
   enum status: { draft: 0, published: 1 }
 
