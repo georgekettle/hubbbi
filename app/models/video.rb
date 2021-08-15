9477c2cfb027ad3f419951475d72b7d56b7e765a
George Kettle <georgekettle.gk@gmail.com>
2021-08-15 12:56:10 +1000

    Vimeo and Youtube api keys added

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,7 @@
 class Video < ApplicationRecord
+  VideoInfo.provider_api_keys = { youtube: ENV["YOUTUBE_API_TOKEN"], vimeo: ENV["VIMEO_API_TOKEN"] }
+  VideoInfo.disable_providers = %w[Dailymotion Wistia]
+
   has_one :section_element, :as =>:element
   has_one :section, through: :section_element
   has_one :page, through: :section
@@ -8,13 +11,5 @@ class Video < ApplicationRecord
   def info
     return nil unless url.present?
     video = VideoInfo.new(url)
-    # set_thumbnails(video) if video.provider == "YouTube"
-    # video
   end
-
-  private
-
-  # def set_thumbnails(video)
-  #   video.thumbnail_large = "https://i.ytimg.com/vi/#{video.video_id}/maxresdefault.jpg"
-  # end
 end
