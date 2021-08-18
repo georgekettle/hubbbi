2331a324104a4f9846d42dafc88e77c3329368f4
George Kettle <georgekettle.gk@gmail.com>
2021-08-18 16:25:06 +1000

    added breadcrumbs stimulus controller to show extra breadcrumbs on click

--- app/javascript/controllers/breadcrumbs_controller.js
+++ app/javascript/controllers/breadcrumbs_controller.js
@@ -1,7 +1,11 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  connect() {
-
+  static targets = ["breadcrumb", "toggle"]
+  expandNav() {
+    this.breadcrumbTargets.forEach((breadcrumb) => {
+      breadcrumb.classList.remove('hidden')
+    })
+    this.toggleTarget.classList.add('hidden')
   }
 }
