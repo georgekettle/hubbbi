3c091ced9da22c365933d2bcc336fc854b5a696f
Miguel Ferrer Isaza <mferrer@hey.com>
2021-08-17 23:12:07 -0500

    remove autogenerated tests

--- test/controllers/courses_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class CoursesControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get courses_show_url
-    assert_response :success
-  end
-end
