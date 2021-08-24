48eee0496eefd589e1dd283f04b2605422c54894
George Kettle <georgekettle.gk@gmail.com>
2021-08-24 14:14:18 +1000

    invitations & group members controller added set selected group

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,9 +1,18 @@
 class Users::InvitationsController < Devise::InvitationsController
+  before_action :hide_all_navbars
+
   def update
     super
   end
 
   def edit
     super
+  end
+
+  private
+
+  def hide_all_navbars
+    hide_navbar
+    hide_desktop_navbar
   end
 end
