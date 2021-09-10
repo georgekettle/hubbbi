0d5357e3a5d8871b77555ed34dcca797bc37054a
Miguel Ferrer Isaza <mferrer@hey.com>
2021-09-09 23:05:37 -0500

    try to fix ci error

--- db/schema.rb
+++ db/schema.rb
@@ -210,19 +210,8 @@ ActiveRecord::Schema.define(version: 2021_09_09_051355) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "full_name"
     t.string "display_name"
-    t.string "invitation_token"
-    t.datetime "invitation_created_at"
-    t.datetime "invitation_sent_at"
-    t.datetime "invitation_accepted_at"
-    t.integer "invitation_limit"
-    t.string "invited_by_type"
-    t.bigint "invited_by_id"
-    t.integer "invitations_count", default: 0
     t.bigint "selected_group_member_id"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
-    t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
-    t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["selected_group_member_id"], name: "index_users_on_selected_group_member_id"
   end
