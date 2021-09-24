e93050d6ea9ebb3d1af71f7cf9f62f7e71e17d99
George Kettle <georgekettle.gk@gmail.com>
2021-09-24 20:39:31 +1000

    updated all remaining dropdowns to implement new dropdown

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -4,18 +4,19 @@ import tippy from 'tippy.js';
 export default class extends Controller {
   static targets = ["toggle", "menu"]
   static values = {
-    hiddenClasses: Array,
-    open: Boolean
+    placement: String,
   }
 
   connect () {
+    this.placementValue ||= 'bottom-end'
+
     tippy(this.toggleTargets, {
       content: this.menuTarget,
       arrow: false,
       interactive: true,
       moveTransition: 'transform 0.2s ease-out',
       offset: [0, 0],
-      placement: 'bottom-end',
+      placement: this.placementValue,
       trigger: 'mouseenter click',
       triggerTarget: this.toggleTargets,
     });
