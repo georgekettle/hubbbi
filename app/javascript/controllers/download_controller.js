dbf3c6f6248a631d22042bef7764e63e7383a931
George Kettle <georgekettle.gk@gmail.com>
2021-10-02 10:16:43 +1000

    FileSaver.js added to enable file download via JS

--- app/javascript/controllers/download_controller.js
+++ app/javascript/controllers/download_controller.js
@@ -1,11 +1,23 @@
 import { Controller } from '@hotwired/stimulus'
+import { saveAs } from 'file-saver'
 
 export default class extends Controller {
+  static values = {
+    name: String
+  }
+
   connect() {
     console.log('download controller')
   }
 
-  download() {
+  download(e) {
+    e.preventDefault()
     // download this.element.src
+    // saveAs(this.element.href, "image.jpg")
+    if (this.hasNameValue) {
+      saveAs(this.element.href, this.nameValue)
+    } else {
+      saveAs(this.element.href)
+    }
   }
 }
