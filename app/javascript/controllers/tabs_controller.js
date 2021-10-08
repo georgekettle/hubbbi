3bc9b557b0256152140615ccc073f366cf622615
George Kettle <georgekettle.gk@gmail.com>
2021-10-08 15:25:06 +1100

    Added HTML example above tabs stimulus controller

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -1,3 +1,32 @@
+// <div data-controller="tabs" data-tabs-active-tab="-mb-px border-l border-t border-r rounded-t">
+//   <ul class="list-reset flex border-b">
+//     <li class="-mb-px mr-1" data-tabs-target="tab" data-action="click->tabs#change">
+//       <a class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-700 font-semibold no-underline" href="#">Active</a>
+//     </li>
+//     <li class="mr-1" data-tabs-target="tab" data-action="click->tabs#change">
+//       <a class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-700 font-semibold no-underline" href="#">Tab</a>
+//     </li>
+//     <li class="mr-1" data-tabs-target="tab" data-action="click->tabs#change">
+//       <a class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-700 font-semibold no-underline" href="#">Tab</a>
+//     </li>
+//     <li class="mr-1">
+//       <a class="bg-white inline-block py-2 px-4 text-gray-300 font-semibold no-underline" href="#">Tab</a>
+//     </li>
+//   </ul>
+
+//   <div class="hidden py-4 px-4 border-l border-b border-r" data-tabs-target="panel">
+//     Tab panel 1
+//   </div>
+
+//   <div class="hidden py-4 px-4 border-l border-b border-r" data-tabs-target="panel">
+//     Tab panel 2
+//   </div>
+
+//   <div class="hidden py-4 px-4 border-l border-b border-r" data-tabs-target="panel">
+//     <iframe width="560" height="315" src="https://www.youtube.com/embed/y3niFzo5VLI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+//   </div>
+// </div>
+
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
