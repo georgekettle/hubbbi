79a397a5dade20a6840d44759ec90c4cfcd5ed39
George Kettle <georgekettle.gk@gmail.com>
2021-10-09 16:57:48 +1100

    Styled bold header above video

--- app/javascript/controllers/video_controller.js
+++ app/javascript/controllers/video_controller.js
@@ -2,10 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 import Plyr from 'plyr';
 
 export default class extends Controller {
+  static targets = ['title', 'container']
+
   initialize() {
-    this.player = new Plyr(this.element, {
+    this.player = new Plyr(this.containerTarget, {
       ratio: '16:9',
       captions: {active: true}
+    });
+
+    const _this = this
+    this.player.on('play', event => {
+      _this.titleTarget.classList.add("hidden")
     });
   }
 }
