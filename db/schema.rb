b289f638e9574fc7b9d173f006ab4aef729cae8d
George Kettle <georgekettle.gk@gmail.com>
2021-10-09 11:26:55 +1100

    Added data field (jsonb) to sections

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_19_104547) do
+ActiveRecord::Schema.define(version: 2021_10_09_002317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -175,6 +175,7 @@ ActiveRecord::Schema.define(version: 2021_09_19_104547) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "section_type", default: 0
+    t.jsonb "data"
     t.index ["page_id"], name: "index_sections_on_page_id"
   end
 
