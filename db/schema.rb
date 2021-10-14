45b6d0918b4c172180f3e1ef0d6435e2779171aa
George Kettle <georgekettle.gk@gmail.com>
2021-10-14 22:12:57 +1100

    added description to courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_14_050658) do
+ActiveRecord::Schema.define(version: 2021_10_14_111128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2021_10_14_050658) do
     t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
     t.index ["group_id"], name: "index_courses_on_group_id"
     t.index ["page_id"], name: "index_courses_on_page_id"
   end
