d3e836f5e25dff6d802e208eb77a961db9d3e366
Miguel Ferrer Isaza <mferrer@hey.com>
2021-10-17 14:20:16 -0500

    try random fixes from the internet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_10_054238) do
+ActiveRecord::Schema.define(version: 2021_10_17_025433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2021_10_10_054238) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "subdomain"
   end
 
   create_table "images", force: :cascade do |t|
