c10e8448eab580fd4a616220bfa59af61899af25
Miguel Ferrer Isaza <mferrer@hey.com>
2021-10-18 15:19:48 -0500

    validate subdomain and add task for populating subdomains for existing groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -8,10 +8,18 @@ class Group < ApplicationRecord
 
   has_one_attached :cover
 
-  validates :name, presence: true
+  validates :name, :subdomain, presence: true
+  validates :subdomain, uniqueness: true
+  validates :subdomain,
+            format: { with: %r{\A[a-z](?:[a-z0-9-]*[a-z0-9])?\z}i, message: "%{value} is not a valid subdomain" },
+            length: { in: 1..63 }
 
   # used for invitable
   def add_user(user)
     GroupMember.create(user: user, group: self) unless GroupMember.find_by(user: user, group: self)
+  end
+
+  def set_subdomain
+    update subdomain: name.parameterize(separator: '')
   end
 end
