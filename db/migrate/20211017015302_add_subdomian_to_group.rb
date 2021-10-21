238a6eb65460ea7bbfa4ef2731dae9a02ee6f6e2
George Kettle <georgekettle.gk@gmail.com>
2021-10-22 10:20:06 +1100

    Update migration

--- db/migrate/20211017015302_add_subdomian_to_group.rb
+++ db/migrate/20211017015302_add_subdomian_to_group.rb
@@ -2,7 +2,7 @@ class AddSubdomianToGroup < ActiveRecord::Migration[6.1]
   def change
     add_column :groups, :subdomain, :string
     Group.all.each do |group|
-      group.update_column :subdomain, group.name.gsub(/\s+/, "")
+      group.update_column :subdomain, group.name.parameterize(separator: '')
     end
   end
 end
