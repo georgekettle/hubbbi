a60394fa3b653aff520b76abf20f557a6633fcc7
George Kettle <georgekettle.gk@gmail.com>
2021-10-22 09:37:47 +1100

    edited migrate file to update existing groups to have subdomain that matches group name

--- db/migrate/20211017015302_add_subdomian_to_group.rb
+++ db/migrate/20211017015302_add_subdomian_to_group.rb
@@ -1,5 +1,8 @@
 class AddSubdomianToGroup < ActiveRecord::Migration[6.1]
   def change
     add_column :groups, :subdomain, :string
+    Group.all.each do |group|
+      group.update_column :subdomain, group.name.gsub(/\s+/, "")
+    end
   end
 end
