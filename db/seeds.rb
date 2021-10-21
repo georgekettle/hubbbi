3ee028f983a04bcd9f8a2026cf2c427f85c1d41e
George Kettle <georgekettle.gk@gmail.com>
2021-10-22 09:37:00 +1100

    Added subdomain to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ puts "Creating faker users"
 puts "Finished creating faker users"
 
 puts "Creating groups"
-  yogi_bears_group = Group.create!(name: "Yogi bears")
+  yogi_bears_group = Group.create!(name: "Yogi bears", subdomain: 'yogibears')
 
   logo_image = 'https://images.unsplash.com/photo-1603988363607-e1e4a66962c6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80'
   logo = URI.open(logo_image)
