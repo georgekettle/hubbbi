6d0a76a557fe78a96a2fabe473ce384b4b835dc9
George Kettle <georgekettle.gk@gmail.com>
2021-10-26 21:17:15 +1100

    Chapter popup showing

--- app/javascript/controllers/video_controller.js
+++ app/javascript/controllers/video_controller.js
@@ -3,7 +3,7 @@ import Plyr from 'plyr';
 import tippy from 'tippy.js';
 
 export default class extends Controller {
-  static targets = ['title', 'container', 'chapter']
+  static targets = ['title', 'container']
   static values = {
     vimeoHash: String
   }
@@ -24,50 +24,55 @@ export default class extends Controller {
     this.player.on('ready', event => {
       _this.plyrTooltip = _this.element.querySelector('.plyr__tooltip')
       if (_this.player.isVimeo) {
-        _this.player.embed.getChapters().then(function(chapters) {
-            // chapters = an array of chapters objects
-            const progressBar = _this.element.querySelector('.plyr__progress')
-            const chaptersHTML = _this.createVimeoChapters(chapters)
-            progressBar.insertAdjacentHTML('beforeend', chaptersHTML)
-        }).catch(function(error) {
-            // an error occurred
-            console.log(error)
-        });
+        _this.initVimeoChapters()
       }
     });
   }
 
-  chapterTargetConnected(element) {
+  initVimeoChapters() {
     const _this = this
-    const chapter = JSON.parse(element.dataset.chapter)
-    const chapterTippy = tippy(element, {
-      content: chapter.title,
-      arrow: true,
-      onShow(instance) {
-        _this.plyrTooltip.classList.add('hidden')
-      },
-      onHide(instance) {
-        _this.plyrTooltip.classList.remove('hidden')
-      },
+    this.player.embed.getChapters().then(function(chapters) {
+        // chapters = an array of chapters objects
+        const progressBar = _this.element.querySelector('.plyr__progress')
+        const chaptersHTML = _this.createVimeoChapters(chapters)
+        progressBar.insertAdjacentHTML('beforeend', chaptersHTML)
+        // setup event listeners to hide time tooltip when hover
+        const chapterElements = _this.element.querySelectorAll('#chapter')
+        chapterElements.forEach((elem) => {
+          elem.addEventListener('mouseout', _this.showPlayerTooltip.bind(_this))
+          elem.addEventListener('mouseover', _this.hidePlayerTooltip.bind(_this))
+          elem.addEventListener('click', (e) => {
+            const currentTime = e.currentTarget.dataset.startTime
+            _this.player.currentTime = parseInt(currentTime)
+          })
+        })
+    }).catch(function(error) {
+        // an error occurred
+        console.log(error)
     });
-    this.chapterTippys.push(chapterTippy)
   }
 
-  chapterTargetDisconnected(element) {
-    this.chapterTippys.forEach((tippyElement) => {
-      tippyElement.destroy()
-    })
+  hidePlayerTooltip(e) {
+    this.plyrTooltip.classList.add('hidden')
+  }
+
+  showPlayerTooltip(e) {
+    this.plyrTooltip.classList.remove('hidden')
   }
 
   createVimeoChapters(chapters) {
     let html = ''
     chapters.forEach((chapter) => {
+      console.log(chapter)
       const position = chapter.startTime / this.player.duration * 100
-      html += `<div class="relative inline-block pointer-events-auto flex-shrink-0" style="left: ${position}%" id="chapter" data-video-target="chapter" data-chapter='${JSON.stringify(chapter)}'>
-            <div class="group rounded-full p-1 pointer-events-auto">
-              <div class="rounded-full h-1 w-1 bg-white transform group-hover:w-3 group-hover:h-3 duration-100 pointer-events-auto"></div>
-            </div>
-          </div>`
+      html += `<div class="group inline-block relative flex-shrink-0" style="left: ${position}%">
+                <span class="plyr__vimeo-chapter group-hover:opacity-100 transform -translate-x-1/2">${chapter.title}</span>
+                <div class="relative inline-block pointer-events-auto flex-shrink-0">
+                  <div class="rounded-full p-1 pointer-events-auto" id="chapter" data-start-time="${chapter.startTime}">
+                    <div class="rounded-full h-1 w-1 bg-white transform group-hover:w-3 group-hover:h-3 duration-100 pointer-events-auto"></div>
+                  </div>
+                </div>
+              </div>`
     })
     html = '<div class="absolute z-10 top-1/2 transform -translate-y-1/2 flex items-center w-full pointer-events-none">' + html + '</div>'
     return html
