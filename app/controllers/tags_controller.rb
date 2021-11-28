102438e5bbfec28bdde354fc817d5bf0d377da19
george kettle <georgekettle.gk@gmail.com>
2021-11-28 19:19:23 +1100

    Display video results for tags page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,5 +2,6 @@ class TagsController < ApplicationController
   def index
     @pages = policy_scope(Page.tagged_with(params[:name]))
     @audios = policy_scope(Audio.tagged_with(params[:name]))
+    @videos = policy_scope(Video.tagged_with(params[:name]))
   end
 end
